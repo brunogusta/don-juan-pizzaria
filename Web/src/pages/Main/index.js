e5eb49e79b8ca387b9b5b956f09ac937cdbfd481
Bruno Gustavo <brugucolosil95@gmail.com>
2019-09-15 04:46:35 -0300

    implemented orders load in main page

--- Web/src/pages/Main/index.js
+++ Web/src/pages/Main/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  formatRelative, parseISO,
+} from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
 import history from '../../routes/history';
+import api from '../../services/api';
 
 import {
   Container,
@@ -14,97 +20,143 @@ import {
   HeaderBox,
   OrderItems,
   Item,
+  OrderFooter,
+  NoOrders,
 } from './styles';
 import LogoImg from '../../images/logo@3x.png';
-import ItemImage from '../../images/1.png';
-import Coke from '../../images/coca-600x540.png';
 
 import { logout } from '../../services/auth';
 
 const Main = () => {
+  const [userOrder, useUserOrder] = useState({
+    cards: [],
+  });
+
   const LogOut = () => {
     logout();
 
     history.push('/');
   };
 
+  const LoadOrders = (data) => {
+    useUserOrder({
+      cards: data,
+    });
+  };
+
+  useEffect(async () => {
+    const { data } = await api.get('/orders').catch(err => console.log(err));
+
+    const formated = data.map((item, index) => {
+      const firstDate = parseISO(item.order.orderDate);
+
+
+      const date = formatRelative(firstDate, new Date(), { locale: ptBR });
+
+
+      const newData = {
+        key: item._id,
+        user: item.user,
+        orderNumber: index + 1,
+        orderDate: date,
+        totalCost: item.order.totalCost,
+        observations: item.order.observations,
+        items: item.order.items,
+      };
+
+      return newData;
+    });
+
+    formated.reverse();
+
+    console.log(formated);
+
+
+    LoadOrders(formated);
+  }, []);
 
   return (
     <Container>
-      <Header>
-        <LogoBox>
-          <img src={LogoImg} alt="logo" />
-          <h1>Pizza Hut</h1>
-        </LogoBox>
-        <UserBox>
-          <UserDetail>
-            <p>Bruno Gustavo</p>
-            <button onClick={LogOut} type="button">Sair do app</button>
-          </UserDetail>
-          <Border />
-          <IconBox>
-            <i className="fas fa-shopping-bag" />
-            <div />
-          </IconBox>
-        </UserBox>
-      </Header>
-      <OrdersContainer>
-        <h2>Últimos pedidos</h2>
-        <OrderBox>
-          <HeaderBox>
-            <p>Pedido #1 - Fernando Hashel</p>
-            <p>há 2 segundos</p>
-            <h3>R$42,00</h3>
-          </HeaderBox>
-          <OrderItems>
-            <Item>
-              <div>
-                <img src={ItemImage} alt="pizza" />
-                <div>
-                  <p>Teste</p>
-                  <p>Teste</p>
-                </div>
-              </div>
-            </Item>
-            <Item>
-              <div>
-                <img src={ItemImage} alt="pizza" />
-                <div>
-                  <p>Teste</p>
-                  <p>Teste</p>
-                </div>
-              </div>
-            </Item>
-            <Item>
-              <div>
-                <img src={ItemImage} alt="pizza" />
-                <div>
-                  <p>Teste</p>
-                  <p>Teste</p>
-                </div>
-              </div>
-            </Item>
-            <Item>
-              <div>
-                <img src={Coke} alt="pizza" />
-                <div>
-                  <p>Teste</p>
-                  <p>Teste</p>
-                </div>
-              </div>
-            </Item>
-            <Item>
-              <div>
-                <img src={Coke} alt="pizza" />
-                <div>
-                  <p>Teste</p>
-                  <p>Teste</p>
-                </div>
-              </div>
-            </Item>
-          </OrderItems>
-        </OrderBox>
-      </OrdersContainer>
+      {userOrder.cards.length === 0
+        ? (
+          <>
+            <Header>
+              <LogoBox>
+                <img src={LogoImg} alt="logo" />
+                <h1>Pizza Hut</h1>
+              </LogoBox>
+              <UserBox>
+                <UserDetail>
+                  <p>Bruno Gustavo</p>
+                  <button onClick={LogOut} type="button">Sair do app</button>
+                </UserDetail>
+                <Border />
+                <IconBox>
+                  <i className="fas fa-shopping-bag" />
+                  {userOrder.cards.length !== 0 && <div />}
+                </IconBox>
+              </UserBox>
+            </Header>
+            <NoOrders>
+              <h1>Não há nenhum pedido.</h1>
+            </NoOrders>
+          </>
+        )
+        : (
+          <>
+            <OrdersContainer>
+              <h2>Últimos pedidos</h2>
+              {userOrder.cards.map(item => (
+                <OrderBox key={item.key}>
+                  <HeaderBox>
+                    <p>
+                Pedido #
+                      {item.orderNumber}
+                      {' '}
+                  -
+                      {' '}
+                      {item.user}
+                    </p>
+                    <p>{item.orderDate}</p>
+                    <h3>
+                R$
+                      {item.totalCost}
+                    </h3>
+                  </HeaderBox>
+                  <OrderItems>
+                    {item.items.flat().map(item => (
+                      <Item>
+                        <div>
+                          <img src={`http://localhost:3002/files/${item.image}`} alt="pizza" />
+                          <div>
+                            <p>
+                        Pizza de
+                              {' '}
+                              {item.name}
+                            </p>
+                            <p>
+                        Tamanho:
+                              {' '}
+                              {item.size}
+                            </p>
+                          </div>
+                        </div>
+                      </Item>
+                    ))}
+                  </OrderItems>
+                  <OrderFooter>
+                    <p>
+                Observações:
+                      {' '}
+                      {item.observations}
+                    </p>
+                  </OrderFooter>
+                </OrderBox>
+              ))}
+            </OrdersContainer>
+          </>
+        )
+      }
     </Container>
   );
 };
